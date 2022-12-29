30d3beda0a23da7055006c29e0d664e1f571bb98
Isaac Nino <isaacdavidnino@gmail.com>
2022-12-29 14:49:36 -0600

    added comments to js file
    changed /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 //DO NOT FORGET TO MAKE MEANIGUL COMMITS AND LEAVE BEHIND HELPFUL COMMENTS// 
 
+//the height properties are made up of both a number value, and a string value//
+//some type properties are enclosed in brackets, to allow multiple values//
+
 let pokemonList = [
     { name: 'Ninetails', types: 'Fire', height: [1.1, 'm'], attack: 76, defense: 75, speed: 100},
     { name: 'Beedrill', type: ['Bug', 'Poison'], height: [1, 'm'], attack: 90, defense: 40, speed: 75},
