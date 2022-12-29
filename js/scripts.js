687045b44d01d1bb85ed0d9c344fc6185eaaa907
Isaac Nino <isaacdavidnino@gmail.com>
2022-12-29 14:33:55 -0600

    created a variable & added array of Pokémon
    changed /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
+//DO NOT FORGET TO MAKE MEANIGUL COMMITS AND LEAVE BEHIND HELPFUL COMMENTS// 
 
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Ninetails', types: 'Fire', height: [1.1, 'm'], attack: 76, defense: 75, speed: 100},
+    { name: 'Beedrill', type: ['Bug', 'Poison'], height: [1, 'm'], attack: 90, defense: 40, speed: 75},
+    { name: 'Alakazam', type: 'Psychic', height: [1.5, 'm'], attack: 50, defense: 45, speed: 120},
+    { name: 'Scyther', type: ['Bug', 'Flying'], height: [1.5, 'm'], attack: 110, defense: 80, speed: 105},
+    { name: 'Kabutops', type: ['Water', 'Rock'], height: [1.3, 'm'], attack: 115, defense: 105, speed: 80}
+];
