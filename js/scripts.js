f28cf47a3961ed6c3997ec12d88984f104e9d14e
Isaac Nino <isaacdavidnino@gmail.com>
2023-01-13 16:57:24 -0600

    updated my scripts.js file to add a forEach loop
    changed /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@
 
 let pokemonList = [
     { name: 'Ninetails', types: 'Fire', height: 1.1, attack: 76, defense: 75, speed: 100}, //medium
-    { name: 'Beedrill', types: ['Bug', 'Poison'], height: .9, attack: 90, defense: 40, speed: 75}, //small
+    { name: 'Beedrill', types: ['Bug', ' Poison'], height: .9, attack: 90, defense: 40, speed: 75}, //small
     { name: 'Alakazam', types: 'Psychic', height: 1.5, attack: 50, defense: 45, speed: 120}, //big
-    { name: 'Scyther', types: ['Bug', 'Flying'], height: 1.5, attack: 110, defense: 80, speed: 105}, //big
-    { name: 'Kabutops', types: ['Water', 'Rock'], height: 1.3, attack: 115, defense: 105, speed: 80} //medium
+    { name: 'Scyther', types: ['Bug', ' Flying'], height: 1.5, attack: 110, defense: 80, speed: 105}, //big
+    { name: 'Kabutops', types: ['Water', ' Rock'], height: 1.3, attack: 115, defense: 105, speed: 80} //medium
 ];
 
 //the closing else clause does not need a condition as it assumes reference to the data that has not been pointed to
 
-for (let i = 0; i < pokemonList.length; i++) {
+/*for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height >= 1.5) {
       document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + 'm),' + ' wow! That is a big Pokemon.' + '<br>')
     }
@@ -23,4 +23,11 @@ for (let i = 0; i < pokemonList.length; i++) {
     else {
        document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + 'm),' + ' that is a small Pokemon.' + '<br>')
     }
-};
+};*/
+
+pokemonList.forEach(function(pokemonList) {
+  document.write(
+    'This is'+ ' ' + pokemonList.name + ',' + ' its height is ' + pokemonList.height + 'm, and its type is ' + pokemonList.types + '. ' 
+    + ' I wouldn\'t get too close, as its attack power is ' + pokemonList.attack + ', its defense is ' + pokemonList.defense + ', and its speed is ' 
+    + pokemonList.speed + '. ' + '<br>');
+});
