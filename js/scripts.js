e447d79afd7ad28bde9d205a20caa214660af23a
Isaac Nino <isaacdavidnino@gmail.com>
2023-01-15 13:57:53 -0600

    updated scripts file to add IIFE and updated forEach loop
    changed /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 //the height properties is made up of a number value
 //some type properties are enclosed in brackets, to allow multiple values
 
+let pokemonRepository = (function () {
 let pokemonList = [
     { name: 'Ninetails', types: 'Fire', height: 1.1, attack: 76, defense: 75, speed: 100}, //medium
     { name: 'Beedrill', types: ['Bug', ' Poison'], height: .9, attack: 90, defense: 40, speed: 75}, //small
@@ -11,7 +12,21 @@ let pokemonList = [
     { name: 'Kabutops', types: ['Water', ' Rock'], height: 1.3, attack: 115, defense: 105, speed: 80} //medium
 ];
 
-//the closing else clause does not need a condition as it assumes reference to the data that has not been pointed to
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+//the closing else clause does not need a condition as it assumes reference to the data that has not been pointed to (old loop)
 
 /*for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height >= 1.5) {
@@ -25,9 +40,11 @@ let pokemonList = [
     }
 };*/
 
-pokemonList.forEach(function(pokemonList) {
+
+//adjusted forEach loop to tap into local state of pokemonList variable
+pokemonRepository.getAll().forEach(function(pokemonList){
   document.write(
-    'This is'+ ' ' + pokemonList.name + ',' + ' its height is ' + pokemonList.height + 'm, and its type is ' + pokemonList.types + '. ' 
-    + ' I wouldn\'t get too close, as its attack power is ' + pokemonList.attack + ', its defense is ' + pokemonList.defense + ', and its speed is ' 
-    + pokemonList.speed + '. ' + '<br>');
+  'This is'+ ' ' + pokemonList.name + ',' + ' its height is ' + pokemonList.height + 'm, and its type is ' + pokemonList.types + '. ' 
+  + ' I wouldn\'t get too close, as its attack power is ' + pokemonList.attack + ', its defense is ' + pokemonList.defense + ', and its speed is ' 
+  + pokemonList.speed + '. ' + '<br>');
 });
