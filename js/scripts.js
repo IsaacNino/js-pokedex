79d242c36d8989eebb56e4f4ddf5a6e1bcfdf208
Isaac Nino <isaacdavidnino@gmail.com>
2023-02-09 21:32:46 -0600

    adjusted height metric, added functions for abilities + types
    changed /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,7 @@ let pokemonRepository = (function() {
       json.results.forEach(function (item) {
         let pokemon = {
           name: item.name,
-          detailsUrl: item.url,
+          detailsUrl: item.url
         };
         add(pokemon);
       });
@@ -57,6 +57,20 @@ let pokemonRepository = (function() {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
+      
+        let arrayOfTypes = [];
+        details.types.forEach(function (item) {
+          arrayOfTypes.push(item.type.name);
+        });
+        // Defining separator between printed array items
+        item.types = arrayOfTypes.join(', ');
+
+        let arrayOfAbilities = [];
+        details.abilities.forEach(function (item) {
+          arrayOfAbilities.push(item.ability.name);
+        });
+        item.abilities = arrayOfAbilities.join(', ');
+    
     }).catch(function (e) {
       console.error(e);
     });
@@ -69,6 +83,9 @@ let pokemonRepository = (function() {
       
         modalTitle.innerHTML = '';
         modalBody.innerHTML = '';
+      
+        let nameElement = document.querySelector('.modal-title');
+        nameElement.innerText = item.name.charAt(0).toUpperCase() + item.name.slice(1);
 
         let modalText = document.createElement('div');
         modalText.classList.add('modal-text');
@@ -78,10 +95,13 @@ let pokemonRepository = (function() {
         sprite.src = item.imageUrl;
       
         let heightElement = document.createElement('p');
-        heightElement.innerText = "Height:" + item.height + " m" ;
+        heightElement.innerText = "Height: " + item.height / 10 + "m" ;
 
         let typesElement = document.createElement('p');
         typesElement.innerText = "Types: " + item.types;
+      
+        let abilitiesElement = document.createElement('p');
+        abilitiesElement.innerText = "Abilities: " + item.abilities;
 
         modalBody.appendChild(sprite);
         modalBody.appendChild(modalText);
